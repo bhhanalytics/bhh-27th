c15aa0e85303b24e7605f4dd95b89fcf2fd1d183
chalermwat <chalermwat.ta@gmail.com>
2024-08-21 16:51:03 +0700

    update card thnak you v.1.1.2

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -11,7 +11,7 @@ export default function FooterComponent(props){
                 <Row style={{width:'95%',display:'flex',justifyContent:'center'}}>
                     <Col span={24} >
                         <Text style={{fontSize:'12px'}}>
-                            Currently V.1.0.1 Copyright © 2024  <br/> 
+                            Currently {import.meta.env.VITE_VERSION} Copyright © 2024 Anniversary 27<sup>th</sup> Bangkokhatyai <br/> 
                             Data Analytics and Innovation Centre, Bangkok Hospital Hatyai. All rights reserved.<br/>
                             {/* Admin: Chalermwat Tanghiransatian, Contact Email:chalermwat.ta@bdms.co.th Tel: 074-27-2800 Ext. 2355 <br/> */}
                         </Text>
